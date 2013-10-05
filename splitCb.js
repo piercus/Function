270d9d4f1a5a1a66947b35f2d27bc4d4d42a8697
Pierre Colle <piercus@gmail.com>
2013-10-05 15:57:15 +0200

    add splitCb

---
+++ splitCb.js
@@ -0,0 +1,27 @@
+var rjs = require('requirejs');
+rjs.define("Function/splitCb",function(){
+  Function.prototype.splitCb = function(n) {
+    var self = this,
+      args1 = Array.prototype.slice.call(arguments);
+    var cbs = [],
+        results = [],
+        errors = [],
+        finalCb = this,
+        finished = 0;
+
+    for(var i = 0; i < n; i++){
+      cbs.push(function(err, o){
+          finished++;
+          
+          if(err) errors.push(err);
+          results.push(o);
+
+          if(finished === n){
+            finalCb(errors.length === 0 ? null : errors, results);
+          }
+      });
+    }
+
+    return cbs;
+  };
+});
